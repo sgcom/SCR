57f98c41798b64b5f203852c04c739a31e4633d2
Stefan Ganev <avatar@sganev.com>
2012-11-03 18:19:09 -0700

    Cleaned up the script slightly.

--- light-sensor-msp430-simple/data/capturedData_3_script.R
+++ light-sensor-msp430-simple/data/capturedData_3_script.R
@@ -7,13 +7,24 @@
 folder = "C:/opt/GitHub/SCR/light-sensor-msp430-simple/light-sensor-receiver-python/data"
 setwd(folder)
 dataset = read.csv("capturedData_3.csv", header=T)
-head(dataset)
-#light = dataset[,2]
-light.ts = ts(dataset[,2], start=0, deltat=5)
-plot(light.ts, type='l', lwd=1, xlab="time [sec]", ylab="light illuminance", main="Light sensor data sample 3", col=hsv(0,1,1,.66))
 
+# as timeseries
+light.ts = ts(dataset[,2], start=0, deltat=5)
 #
-# smoothed 
-#
+# smoothed
 light.ts.f1 = filter(light.ts, filter=rep(1/7,7))
-lines(light.ts.f1, type='l', lty=4, lwd=1, xlab="time [sec]", ylab="light illuminance", main="Light sensor data - smoothed", col=hsv(.7,1,1,.66))
+#
+# plot the raw measurements and the smoothed series on top of it
+#
+plot(light.ts, 
+     type='l', 
+     xlab="time [sec]", 
+     ylab="light illuminance", 
+     main="Light sensor data sample 3", 
+     col=hsv(0,1,1,.66))
+lines(light.ts.f1, 
+      type='l', 
+      lty=4, 
+      col=hsv(.7,1,1,.66))
+
+#lines(light.ts.f1, type='l', lty=4, lwd=1, xlab="time [sec]", ylab="light illuminance", main="Light sensor data - smoothed", col=hsv(.7,1,1,.66))
