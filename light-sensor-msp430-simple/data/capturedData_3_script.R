6e9e121bd2e98076dc7adb5288067678459bf6e6
Stefan Ganev <avatar@sganev.com>
2012-11-03 18:08:03 -0700

    Moved the data directory + minor R script fix.

--- light-sensor-msp430-simple/data/capturedData_3_script.R
+++ light-sensor-msp430-simple/data/capturedData_3_script.R
@@ -9,11 +9,11 @@ setwd(folder)
 dataset = read.csv("capturedData_3.csv", header=T)
 head(dataset)
 #light = dataset[,2]
-light.ts = ts(dataset[,2], start=0, frequency=5)
-plot(light.ts, type='l', xlab="time [sec]", ylab="light illuminance", main="Light sensor data sample 3", col=2)
+light.ts = ts(dataset[,2], start=0, deltat=5)
+plot(light.ts, type='l', lwd=1, xlab="time [sec]", ylab="light illuminance", main="Light sensor data sample 3", col=hsv(0,1,1,.66))
 
 #
 # smoothed 
 #
 light.ts.f1 = filter(light.ts, filter=rep(1/7,7))
-lines(light.ts.f1, type='l', xlab="time [sec]", ylab="light illuminance", main="Light sensor data - smoothed", col=4)
+lines(light.ts.f1, type='l', lty=4, lwd=1, xlab="time [sec]", ylab="light illuminance", main="Light sensor data - smoothed", col=hsv(.7,1,1,.66))
